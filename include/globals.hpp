de11768e4c932127aee758c69c859fa6999fe4da
Kordemc <kordecarter@gmail.com>
2026-01-13 17:47:01 -0600

    Added auto selector

--- include/globals.hpp
+++ include/globals.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include "main.h"
 
+inline bool skills = false;
+inline bool match = true;
+inline bool redAlliance = false;
+inline bool blueAlliance = true;
+inline int autoSelection = 0;
 
 inline const double wheelDiameter = 2.75;
 inline const double trackWidth = 12.5;
--- include/main.h
+++ include/main.h
@@ -35,6 +35,7 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
+#include "liblvgl/lvgl.h"
 
 /**
  * You should add more #includes here
@@ -76,8 +77,11 @@ void opcontrol(void);
  */
 //#include <iostream>
 #include "lib/api.hpp"
+#include "screen.h"
 #include "util/timer.hpp"
 #include "globals.hpp"
+#include "subsystems/intake.hpp"
+
 #endif
 
 #endif  // _PROS_MAIN_H_
