f86d4322a646b71deedfba15037562a3e006c5cf
Brandon Fortes <55511864+MrFortez@users.noreply.github.com>
2026-03-17 17:30:41 -0500

    Fix setPose() bug in chassis class

--- src/lib/chassis.cpp
+++ src/lib/chassis.cpp
@@ -119,6 +119,7 @@ Pose Chassis::getPose() const {
  */
 void Chassis::setPose(const Pose& newPose) { 
     *pose = newPose; 
+    odometry->imu->set_rotation((newPose.getTheta() * (180 / M_PI)) * -1);
 }
 
 /**
@@ -131,6 +132,7 @@ void Chassis::setPose(double x, double y, double theta) {
     pose->setX(x);
     pose->setY(y);
     pose->setTheta(theta);
+    odometry->imu->set_rotation((theta * (180 / M_PI)) * -1);
 }
 
 /**
